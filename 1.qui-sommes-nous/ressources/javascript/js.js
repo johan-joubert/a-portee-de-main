43aa10e3a8de7a39d737a18db7e3183118557b7b
Johan <njoubert06@gmail.com>
2020-10-29 12:00:08 +0100

    add modif on all files

--- 1.qui-sommes-nous/ressources/javascript/js.js
+++ 1.qui-sommes-nous/ressources/javascript/js.js
@@ -1,7 +1,7 @@
 let nav = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
-  if(window.scrollY > 50) {
+  if(window.scrollY > 1) {
     nav.classList.add('scroll');
   }
   else {
