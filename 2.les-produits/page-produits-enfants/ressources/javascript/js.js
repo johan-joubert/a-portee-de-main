f442dabd6ffbb9d660334531b43081d8fb069349
Johan <njoubert06@gmail.com>
2020-10-30 13:49:39 +0100

    add js categorie

--- 2.les-produits/page-produits-enfants/ressources/javascript/js.js
+++ 2.les-produits/page-produits-enfants/ressources/javascript/js.js
@@ -10,3 +10,26 @@ window.addEventListener('scroll', () => {
 })
 
 
+
+
+let title = document.querySelector('#title-taille');
+let categorie = document.querySelector('.liste-categorie');
+
+title.addEventListener("click", () => {
+  if (getComputedStyle(categorie).display != "none"){
+    categorie.style.display = "none";
+  } else {
+    categorie.style.display = "block";
+  }
+});
+
+let title2 = document.querySelector('#title-couleur');
+let categorie2 = document.querySelector('.liste-categorie2');
+
+title2.addEventListener("click", () => {
+  if (getComputedStyle(categorie2).display != "none"){
+    categorie2.style.display = "none";
+  } else {
+    categorie2.style.display = "block";
+  }
+})
